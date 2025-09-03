057ceea8dff7b02f55cebf3841ba77be1e065799
josephinetancy <99933363+josephinetancy@users.noreply.github.com>
2025-09-03 08:54:56 -0700

    updated

--- jsFiles/streakgame.js
+++ jsFiles/streakgame.js
@@ -1,6 +1,6 @@
 //randomAssignment
 
-randomAssignment = 5;
+randomAssignment = 1;
 
 console.log(randomAssignment);
 
@@ -2624,7 +2624,7 @@ variables for plugins
 ///
 */
 
-let noOfTrials = 1; //so 60 in total
+let noOfTrials = 15; //so 60 in total
 
 ///fake participant's activation time for WL and LL trials, that far exceeds trial duration
 let partner_rtL = 20000; //for when partner "loses".
